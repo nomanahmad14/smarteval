d4d2af6dc37b05f6c85f8550ba9c6451c5da17cb
rayyanqureshi0123 <rayyanqureshi1112@gmail.com>
2026-01-08 17:31:03 +0530

    completed teacher model

---
+++ backend/models/teacherModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const teacherSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    image: {
+      type: String,
+      default: "", 
+    },
+
+    subjects: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subject",
+        required: true,
+      },
+    ],
+
+    experience: {
+      type: Number,
+      required: true,
+    },
+
+    address: String,
+    city: String,
+    about: String,
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Teacher =
+  mongoose.models.Teacher || mongoose.model("Teacher", teacherSchema);
+
+export default Teacher;
