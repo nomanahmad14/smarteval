0c63568cdda8f1e79c4b7d660e3a1ae44444a151
Noman Ahmad <noman22221@gmail.com>
2026-01-08 18:30:14 +0530

    created and compeleted attempt model

---
+++ backend/models/attemptModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const attemptSchema = new mongoose.Schema({
+
+  attemptedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",          // your Student model name
+    required: true
+  },
+
+  quiz: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Quiz",
+    required: true
+  },
+
+  answers: [
+    {
+      question: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Question",
+        required: true
+      },
+      selectedOption: {
+        type: String,
+        enum: ["a", "b", "c", "d"],
+        required: true
+      }
+    }
+  ],
+
+  startedAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  submittedAt: {
+    type: Date
+  },
+
+  marksObtained: {
+    type: Number,
+    default: 0
+  },
+
+  isAutoSubmitted: {
+    type: Boolean,
+    default: false
+  }
+
+}, { timestamps: true });
+
+const Attempt =
+  mongoose.models.Attempt || mongoose.model("Attempt", attemptSchema);
+
+export default Attempt;
