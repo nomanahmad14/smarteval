3b1ea956d870b620572049cecdfc0df1a57ce597
Noman Ahmad <noman22221@gmail.com>
2026-01-08 17:32:21 +0530

    compeleted userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+    },
+    gender:{
+        type:String,
+        enum:["male","female","other"]
+    },
+    subjects:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Subject"
+        }
+    ],
+    subscribedTeachers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Teacher"
+    }
+  ]
+
+},{ timestamps: true })
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema)
+
+export default userModel
