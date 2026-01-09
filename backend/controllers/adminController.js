4b78987b586e65a05d87dfaae9375188e0542f9f
Noman Ahmad <noman22221@gmail.com>
2026-01-09 23:54:07 +0530

    compeleted loginAdmin function in admin controller file

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+//api for admin login
+
+const loginAdmin = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        } else {
+            res.json({ success: true, message: "invalid credentials" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+
+
+
+}
+
+export {loginAdmin}
