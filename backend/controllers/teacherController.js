ca49029af03961169013f6dd68feed29f0724dae
rayyanqureshi0123 <rayyanqureshi1112@gmail.com>
2026-01-12 22:37:25 +0530

    created addQuestion function in teacherController

--- backend/controllers/teacherController.js
+++ backend/controllers/teacherController.js
@@ -1,6 +1,7 @@
 import Teacher from "../models/teacherModel.js";
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import Question from "../models/questionModel.js";
 
 // api for teacher login
 
@@ -31,4 +32,71 @@ const loginTeacher = async (req, res) => {
   }
 };
 
-export {loginTeacher}
+
+
+const addQuestion = async (req, res) => {
+  try {
+    const {
+      ques,
+      difficulty,
+      options,
+      correctAnswer,
+      subject
+    } = req.body;
+
+    const teacherId = req.user.id;
+
+    if (!ques || !options || !correctAnswer || !subject) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    if (!options?.a || !options?.b || !options?.c || !options?.d) {
+      return res.status(400).json({
+        success: false,
+        message: "All four options (a, b, c, d) are required",
+      });
+    }
+
+    if (!["a", "b", "c", "d"].includes(correctAnswer)) {
+      return res.status(400).json({
+        success: false,
+        message: "Correct answer must be one of a, b, c, or d",
+      });
+    }
+
+    if (difficulty && !["easy", "medium", "hard"].includes(difficulty)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid difficulty",
+      });
+    }
+
+    const question = await Question.create({
+      ques,
+      difficulty,
+      options,
+      correctAnswer,
+      subject,
+      createdBy: teacherId,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Question added successfully",
+      questionId: question._id,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+
+export {loginTeacher, addQuestion}
--- backend/routes/teacherRouter.js
+++ backend/routes/teacherRouter.js
@@ -1,2 +1,12 @@
+import express from 'express'
+import authAdmin from '../middlewares/authAdmin';
+import { loginTeacher } from '../controllers/teacherController.js';
+import { addTeacher } from '../controllers/adminController.js';
 
+
+const teacherRouter=express.Router();
+
+teacherRouter.post("/addTeacher", authAdmin,addTeacher )
+
+export default teacherRouter
 
