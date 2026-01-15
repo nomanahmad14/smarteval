009b62c963b01f27b3e57a499ec920986421f04e
Noman Ahmad <noman22221@gmail.com>
2026-01-16 00:04:21 +0530

    compelted quiz routes in user router

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,29 @@
-import express from "express"
-import { registerUser, loginUser, } from "../controllers/userController.js"
-import upload from "../middlewares/multer.js";
+import express from "express";
+import {
+  loginUser, registerUser, getSubjectForUser, getQuizzesBySubjectForUser,
+  startQuizAttempt, getAttemptDetails,
+  submitQuizAttempt,autoSubmitQuizAttempt, getAttemptResult, getMyAttemptedQuizzes
+} from "../controllers/userController.js";
 
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register',upload.single("image"), registerUser)
-userRouter.post('/login', loginUser)
+userRouter.post("/register", upload.single("image"), registerUser);
+userRouter.post("/login", loginUser);
 
-export default userRouter
+userRouter.get("/subjects", authUser, getSubjectForUser);
+userRouter.get("/subjects/:subjectId/quizzes", authUser, getQuizzesBySubjectForUser);
+
+userRouter.post("/quiz/:quizId/start", authUser, startQuizAttempt);
+
+userRouter.get("/attempt/:attemptId", authUser, getAttemptDetails);
+userRouter.post("/attempt/:attemptId/submit", authUser, submitQuizAttempt);
+userRouter.post("/attempt/:attemptId/auto-submit", authUser, autoSubmitQuizAttempt);
+
+userRouter.get("/attempt/:attemptId/result", authUser, getAttemptResult);
+userRouter.get("/attempts", authUser, getMyAttemptedQuizzes);
+
+export default userRouter;
+    
