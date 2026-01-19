9456b45e6632922580678adb105a1ebc0540299d
Noman Ahmad <noman22221@gmail.com>
2026-01-19 23:07:06 +0530

    setup admin frontend with tailwind

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-3xl font-bold text-primary">
+        Tailwind Working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
