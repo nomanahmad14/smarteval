58529a9fea9f0cf87a73ba7832a775b73aae8f5d
Noman Ahmad <noman22221@gmail.com>
2026-01-20 23:37:29 +0530

    updated main,jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,22 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 
-import { AuthProvider } from "./context/AuthContext.jsx";
-//import { AdminProvider } from "./context/AdminContext.jsx";
-//import { TeacherProvider } from "./context/TeacherContext.jsx";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        
-            <App />
-          
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
