0e877aaf934d8c1c5a72d34f80c4cc9babffcf43
rayyanqureshi0123 <rayyanqureshi1112@gmail.com>
2026-01-24 16:54:19 +0530

    Added TeacherDashboard file in Teacher folder

---
+++ admin/src/pages/Teacher/TeacherDashboard.jsx
@@ -0,0 +1,11 @@
+const TeacherDashboard = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">
+        Teacher Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default TeacherDashboard;
